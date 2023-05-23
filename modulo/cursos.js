c82a0b25daef68fe87b2527f70d319053c01fa71
MURYLLO SIQUEIRA VIEIRA <22282224@dsn127m1242375.cfp127aluno.local>
2023-05-23 15:05:29 -0300

    atualizando a api

--- modulo/cursos.js
+++ modulo/cursos.js
@@ -1,12 +1,12 @@
 var cursos = [
     {
-        "nome": "001 - Técnico em Desenvolvimento de Sistemas",
+        "nome": "Técnico em Desenvolvimento de Sistemas",
         "sigla": "DS",
         "icone": "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
         "carga": "1200",
     },
     {
-        "nome": "002 - Técnico em Redes de Computadores",
+        "nome": "Técnico em Redes de Computadores",
         "sigla": "RDS",
         "icone": "https://img.icons8.com/ultraviolet/344/thin-client.png",
         "carga": "1200"
